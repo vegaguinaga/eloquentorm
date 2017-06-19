48238336002d13ec24a254f1f4fc8cbe7494bc97
Jose Vega <vegaguinaga@gmail.com>
2017-06-18 18:43:04 -0600

    Eloquent create, update

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,7 +10,27 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+use EloquentORM\User;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/create', function () {
+    $user = User::create([
+            'name' => 'Luis Vega',
+            'email' => 'vega@gmail.com',
+            'password' => bcrypt('jlva'),
+            'gender' => 'm',
+            'biography' => 'Aprendiz de Laravel'
+            
+    ]);
+    return 'Usuario guardado';
+});
+
+Route::get('/update-user', function () {
+    $user = User::find(1);
+    
+    $user->gender = 'm';
+    $user->biography = 'El mero chingon';
+    
+    $user->save();
+    
+    return 'Usuario Actualizado';
 });
