a8837ad1925866789017d98147df371712305a8b
Jose Vega <vegaguinaga@gmail.com>
2017-06-22 09:02:07 -0600

    Delete, Form

--- config/app.php
+++ config/app.php
@@ -144,6 +144,9 @@ return [
         EloquentORM\Providers\AuthServiceProvider::class,
         EloquentORM\Providers\EventServiceProvider::class,
         EloquentORM\Providers\RouteServiceProvider::class,
+        
+        //vendor
+        Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -192,6 +195,15 @@ return [
         'URL'       => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
+        
+        /**
+         * vendor
+         */
+        'Form' => Collective\Html\FormFacade::class,
+        'Html' => Collective\Html\HtmlFacade::class,
+        
+        
+        
 
     ],
 
---
+++ resources/views/pages/delete.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('content')
+<div class="container">
+    <p class="alert alert-success">El usuario se eliminó Correctamente
+    <a href="{{ route('home') }}">Ir al home</a>
+    </p>
+</div>
+@stop
